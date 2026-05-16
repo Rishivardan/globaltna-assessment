e05074fc403d0a1bd6414aa448b79144db5adc7f
V.Rishivardan <rishivardan2004@gmail.com>
2026-05-16 13:36:27 +0530

    CORS configuration updated

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://globaltna-assessment-zeta.vercel.app",
+  ],
+}));
 app.use(express.json());
 
 // Test Route
